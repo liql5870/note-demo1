fb8f5e37689f53a5ccea001bcdcc0cd83bf9ba70
liql <liqianlong219@gmail.com>
2022-02-17 16:54:46 +0800

    修改并完成login组件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,18 +5,24 @@
         <div class="modal-container">
           <div class="main"></div>
           <div class="form">
-            <h3>创建账户</h3>
-            <div v-show="true" class="register">
-              <input type="text" placeholder="用户名">
-              <input type="password" placeholder="密码">
-              <div class="button">创建账号</div>
-            </div>
-            <h3>登录</h3>
-            <div v-show="false" class="login">
-              <input type="text" placeholder="输入用户名">
-              <input type="password" placeholder="密码">
-              <div class="button"> 登录</div>
-            </div>
+            <h3 @click="showRegister">创建账户</h3>
+            <transition name="slide">
+              <div v-bind:class="{show: isShowRegister}" class="register">
+                <input type="text" v-model="register.username" placeholder="用户名">
+                <input type="password" v-model="register.password" @keyup.enter="onRegister" placeholder="密码">
+                <p v-bind:class="{error: register.isError}"> {{ register.notice }}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
+            </transition>
+            <h3 @click="showLogin">登录</h3>
+            <transition name="slide">
+              <div v-bind:class="{show: isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名">
+                <input type="password" v-model="login.password" @keyup.enter="onLogin" placeholder="密码">
+                <p v-bind:class="{error: login.isError}"> {{ login.notice }}</p>
+                <div class="button" @click="onLogin"> 登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -24,8 +30,71 @@
   </div>
 </template>
 
-<script lang='ts'>
-export default {}
+<script>
+export default {
+  data () {
+    return {
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: '',
+        password: '',
+        notice: '输入用户名和密码',
+        isError: false
+      },
+      register: {
+        username: '',
+        password: '',
+        notice: '创建账号后，请记住用户名和密码',
+        isError: false
+      }
+    }
+  },
+
+  methods: {
+    showLogin () {
+      this.isShowLogin = true
+      this.isShowRegister = false
+    },
+    showRegister () {
+      this.isShowLogin = false
+      this.isShowRegister = true
+    },
+    onRegister () {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
+        this.register.isError = true
+        this.register.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.register.password)) {
+        this.register.isError = true
+        this.register.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.register.isError = false
+      this.register.notice = ''
+      console.log(`start register..., username: ${this.register.username} , password: ${this.register.password}`)
+    },
+    onLogin () {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
+        this.login.isError = true
+        this.login.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.login.password)) {
+        this.login.isError = true
+        this.login.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.login.isError = false
+      this.login.notice = ''
+
+      console.log(`start login..., username: ${this.login.username} , password: ${this.login.password}`)
+    }
+  }
+
+}
+
 </script>
 
 <style lang='scss' scoped>
@@ -66,9 +135,11 @@ export default {}
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
+    overflow: hidden;
 
     h3 {
       padding: 10px 20px;
+      margin-top: -1px;
       font-weight: normal;
       font-size: 16px;
       border-top: 1px solid #eee;
@@ -92,8 +163,15 @@ export default {}
     }
 
     .login, .register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height .4s;
+
+      &.show {
+        height: 193px;
+      }
 
       input {
         display: block;
